8529a8adc50d63044ba2b25291e337ab53013e3f
Zaccary Crumpton <zjcrumpton@gmail.com>
2021-06-14 22:19:08 -0600

    implement authentication

--- src/dtos/users.dto.ts
+++ src/dtos/users.dto.ts
@@ -1,9 +1,28 @@
-import { IsString, IsEmail } from 'class-validator';
+import { IsString, IsEmail, Length, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
+  @IsNotEmpty()
   public email: string;
 
   @IsString()
+  @IsNotEmpty()
+  @Length(6, 12)
+  public password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 20)
+  public username: string;
+}
+
+export class LoginUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  public email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 12)
   public password: string;
 }
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import AuthController from '@controllers/auth.controller';
-import { CreateUserDto } from '@dtos/users.dto';
+import { CreateUserDto, LoginUserDto } from '@dtos/users.dto';
 import Route from '@interfaces/routes.interface';
 import authMiddleware from '@middlewares/auth.middleware';
 import validationMiddleware from '@middlewares/validation.middleware';
@@ -16,8 +16,10 @@ class AuthRoute implements Route {
 
   private initializeRoutes() {
     this.router.post(`${this.path}signup`, validationMiddleware(CreateUserDto, 'body'), this.authController.signUp);
-    this.router.post(`${this.path}login`, validationMiddleware(CreateUserDto, 'body'), this.authController.logIn);
-    this.router.post(`${this.path}logout`, authMiddleware, this.authController.logOut);
+    this.router.post(`${this.path}login`, validationMiddleware(LoginUserDto, 'body'), this.authController.logIn);
+
+    // this route exists to expose the authentication middleware for testing
+    this.router.post(`${this.path}authenticate`, authMiddleware, this.authController.authenticate);
   }
 }
 
